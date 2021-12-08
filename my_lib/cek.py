5fc89dd12d8558e0aeaa0f11e274237123fde2be
yere13 <yere13@gmail.com>
2021-12-08 15:41:03 +0700

    first commit

---
+++ my_lib/cek.py
@@ -0,0 +1,8 @@
+class cek:
+   
+    
+    def cek(self,a):
+        if(a%2 == 0)
+           print ("Genap")
+        else
+            print ("Ganjil")
---
+++ my_lib/kalkulator.py
@@ -0,0 +1,21 @@
+class kalkulator:
+    x=0
+    y=0
+    z=0
+    
+    a=0
+    b=0
+    c=0
+    
+    def __init__(self, a,b):
+        self.a=a
+        self.b=b
+        return
+    
+    def kali(self):
+        self.z=self.x*self.y
+        
+    
+    def tambah(self):
+        self.c=self.a+self.b
+        
